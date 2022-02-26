56c08139a0141a68ea78041eae44c54b5eb62f0e
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2022-02-26 22:20:03 +0530

    feat(blog): add Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
-# Create your models here.
+User = get_user_model()
+
+
+def upload_post_image(instance, filename):
+    return f"posts/{instance.user}/{filename}"
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    thumbnail = models.ImageField(upload_to="upload_post_image")
+
+    def __str__(self):
+        return self.title
+
+
+def pre_save_receiver(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+
+pre_save.connect(pre_save_receiver, sender=Post)
